e5a144e75839bc7ece624414b8cca6c424f70481
Tay Yang Shun <tay.yang.shun@gmail.com>
2015-04-17 22:29:03 +0800

    More explanations for Config

--- a3/Config.js
+++ a3/Config.js
@@ -9,10 +9,20 @@ var Config = {
 	SERVER_NAME : "localhost",	// server name of game
 	//SERVER_NAME : "172.28.176.122"	// server name of game
 
-	INTEREST_MANAGEMENT : true,				// Turn on/off interest management
-	DEBUG_MODE : true,							// Enable/Disable rendering skipped events (only work when INTEREST_MANAGEMENT is on)
-	THROUGHPUT_CALCULATION_DURATION : 2000,
-	MAX_ESTIMATE_SEND_RATE_PER_USER : 20,		// Estimate the maximum sending rate per user
+	NUM_ROW: 4,
+	NUM_COL: 4,
+	INTEREST_MANAGEMENT : true,				// 	Turn interest management on/off. 
+											//	`hit` and `fire` events are optimized of set to `true`.
+
+	DEBUG_MODE : true,						// 	Enable/disable rendering rockets that are not of interest to current ship 
+											//	(only works when INTEREST_MANAGEMENT is `true`)
+											//	Rockets are rendered as magenta when set to `true`.
+
+	THROUGHPUT_CALCULATION_DURATION : 2000,	//	Interval range to collect network traffic
+
+	MAX_ESTIMATE_SEND_RATE_PER_USER : 0,	// 	Estimate the maximum sending rate per user. 
+											//	Set to non-zero to enable the feature where extra messages
+											//	can be sent where network is not congested.
 }
 
 
